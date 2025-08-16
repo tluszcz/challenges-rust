5b9dd3ed5cde5d1c7eb4c320432dbae0851df666
Michał Leśnik <michal.lesnik92@gmail.com>
2025-08-16 20:23:25 +0200

    gui bones goofing around

--- ch1/src/main.rs
+++ ch1/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+use std::error::Error;
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let native_options = eframe::NativeOptions::default();
+
+    eframe::run_native(
+        "My egui app", 
+        native_options, 
+        Box::new(|cc| Ok(Box::new(MyEguiApp::new(cc))))
+    )?;
+
+    Ok(())
+}
+
+#[derive(Default)]
+struct MyEguiApp {}
+
+impl MyEguiApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+
+impl eframe::App for MyEguiApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello World!");
+        });
+    }
 }

76bb89f27d440614cc53874d724e35394110f3a5
Michał Leśnik <michal.lesnik92@gmail.com>
2025-08-17 08:45:14 +0200

    request last kb

--- ch1/src/main.rs
+++ ch1/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Error;
+
+async fn get_range_request(url: &str) -> Result<(), Error> {
+    let client = reqwest::Client::new();
+    let response = client
+                            .get(url)
+                            .header("Range", "bytes=-1024")
+                            .send()
+                            .await?;
+
+    println!("Status: {}", response.status());
+
+    let bytes = response.bytes().await?;
+    println!("{}", bytes.len());
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let url = todo!();
+    get_range_request(url).await?;
+    Ok(())
 }
